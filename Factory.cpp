c9de604cc2be6d1267d3170fdbfc61ea3a5c41ee
Davide mattioli <davide.mattioli.2001@gmail.com>
2022-05-02 18:31:07 +0200

    Modified pop variation  function

--- Factory.cpp
+++ Factory.cpp
@@ -1,20 +1,25 @@
 //
 // Created by dadoi on 22/04/2022.
 //
-
+#include <math.h>
 #include "Factory.h"
 #include "Pop.h"
 #include "Market.h"
-
+#include <random>
 #include <iostream>
 #include <stdio.h>
+
+
 
 float Factory::Troughput(){
     float t=(log(investment+10)/log(10000));
     return t;
 };
 void Factory::Production(){
-    int production=round(sqrt(workers->number)+cos(workers->number)+3.5-log(workers->number));
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<double> dist(1.0, 10.0);
+    int production=round(sqrt(workers->number)+cos(workers->number)*dist(mt)+4.5-log(workers->number));
     float money_gained=production*(markets->food_value);
     markets->food_produced=markets->food_produced+production;
     cash=cash+money_gained;
